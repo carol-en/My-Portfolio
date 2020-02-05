315e30ad408cc4d9e0698e79a8dca195ada34b00
Caroline Nolasco <cnolascoix@gmail.com>
2020-02-05 12:43:04 -0800

    working new method wip update method

--- art-marks/art_marks_client/src/components/Bookmarks.js
+++ art-marks/art_marks_client/src/components/Bookmarks.js
@@ -1,10 +1,98 @@
 import React, {Component} from 'react';
 import '../App.css';
 
+class NewForm extends Component {
+    render() {
+        const handleSubmit = this.props.handleSubmit,
+              handleChange = this.props.handleChange,
+              formInputs = this.props.formInputs;
+        return (
+            <>
+                <h1>New Site</h1>
+                <form  onSubmit={handleSubmit}>
+                    <div className="temp-form-name">
+                        <label htmlFor="site_name">Site Name</label>
+                        <input  value={formInputs.site_name} id="site_name" type="text" placeholder="Site Name" onChange={handleChange}/>
+                    </div>
+                    <div className="temp-form-name">
+                        <label htmlFor="url">URL</label>
+                        <input  value={formInputs.url} id="url" type="text" placeholder="URL"onChange={handleChange} />
+                    </div>
+                    <div className="temp-form-name">
+                        <label htmlFor="category">Category</label>
+                        <input  value={formInputs.category} id="category" type="text" placeholder="Category" onChange={handleChange}/>
+                    </div>
+                    <div className="temp-form-name">
+                        <label htmlFor="img">Image</label>
+                        <input  value={formInputs.img} id="img" type="text" placeholder="Image" onChange={handleChange}/>
+                    </div>
+                    <div className="temp-form-name">
+                        <label htmlFor="description">Description</label>
+                        <textarea  value={formInputs.description} id="description" placeholder="Description" onChange={handleChange}></textarea>
+                    </div>
+                    <div className="temp-form-name">
+                        <label htmlFor="temp-change"></label>
+                        <input type="submit" value="New Program!" />
+                    </div>
+                </form>
+            </>
+        )
+    }
+}
+
+class EditForm extends Component {
+    render() {
+        const handleUpdate = this.props.handleUpdate,
+              handleChange = this.props.handleChange,
+              formInputs = this.props.formInputs;
+        return (
+            <>
+                <h1>Edit Site</h1>
+                <form  onSubmit={handleUpdate}>
+                    <div className="temp-form-name">
+                        <label htmlFor="site_name">Site Name</label>
+                        <input  value={formInputs.site_name} id="site_name" type="text" placeholder="Site Name" onChange={handleChange}/>
+                    </div>
+                    <div className="temp-form-name">
+                        <label htmlFor="url">URL</label>
+                        <input  value={formInputs.url} id="url" type="text" placeholder="URL"onChange={handleChange} />
+                    </div>
+                    <div className="temp-form-name">
+                        <label htmlFor="category">Category</label>
+                        <input  value={formInputs.category} id="category" type="text" placeholder="Category" onChange={handleChange}/>
+                    </div>
+                    <div className="temp-form-name">
+                        <label htmlFor="img">Image</label>
+                        <input  value={formInputs.img} id="img" type="text" placeholder="Image" onChange={handleChange}/>
+                    </div>
+                    <div className="temp-form-name">
+                        <label htmlFor="description">Description</label>
+                        <textarea  value={formInputs.description} id="description" placeholder="Description" onChange={handleChange}></textarea>
+                    </div>
+                    <div className="temp-form-name">
+                        <label htmlFor="temp-change"></label>
+                        <input type="submit" value="Update Program!" />
+                    </div>
+                </form>
+            </>
+        )
+    }
+}
+
 
 class Bookmarks extends Component {
     state = {
-        sites: []
+        sites: [],
+        formInputs: {
+            site_name:"",
+            url: "",
+            category: "",
+            description: "",
+            img: ""
+
+        },
+        editing: false
+        
     }
 
     componentDidMount() {
@@ -18,21 +106,75 @@ class Bookmarks extends Component {
         .catch(error => console.log(error));
     }
 
-    handleChange = () => {
-        console.log("Change has been handled");
+    handleChange = (event) => {
+        const updateInput = Object.assign(
+            this.state.formInputs,
+            { [event.target.id]: event.target.value } );
+            this.setState(updateInput);
     }
 
     handleSubmit = (event) => {
         event.preventDefault();
-        console.log("New Entry has been sent!");
+        
+        fetch("http://localhost:3000/bookmarks", {
+                body: JSON.stringify(this.state.formInputs),
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+
+        })
+        .then(newMark => {
+            return newMark.json()
+        })
+        .then(jMark => {
+            this.setState({
+                formInputs: {
+                    site_name:"",
+                    url: "",
+                    category: "",
+                    description: "",
+                    img: ""
+                },
+                sites: [jMark, ...this.state.sites]
+            });
+        })
+        .catch(err => console.log(err));
+
     }
 
-    handleEdit = () => {
-        console.log("Update active!");
+    handleEdit = (entry) => {
+        const editing = this.state.editing;
+        if(!editing) {
+            this.setState({ 
+                editing: !editing,
+                formInputs: {
+                    site_name: entry.site_name,
+                    url: entry.url,
+                    category: entry.category,
+                    description: entry.description,
+                    img: entry.img
+                }
+            }); 
+        }  else if(editing) {
+            this.setState({ 
+                editing: !editing,
+                formInputs: {
+                    site_name: "",
+                    url: "",
+                    category: "",
+                    description: "",
+                    img: ""
+                }
+            });
+        }
     }
 
-    handUpdate = () => {
+    handleUpdate = (event) => {
+        event.preventDefault();
         console.log("Entry updated!");
+        console.log(event);
     }
 
     handleDestroy = () => {
@@ -41,46 +183,28 @@ class Bookmarks extends Component {
 
 
     render() {
-        const sites = this.state.sites;
-
         return (
             <main>
                 <header className="form-area">
-                    <h1>New Site</h1>
-                    <form  onSubmit={this.handleSubmit}>
-                        <div className="temp-form-name">
-                            <label htmlFor="temp-change"></label>
-                            <input type="text" placeholder="Site Name" onChange={this.handleChange}/>
-                        </div>
-                        <div className="temp-form-name">
-                            <label htmlFor="temp-change"></label>
-                            <input type="text" placeholder="URL"onChange={this.handleChange} />
-                        </div>
-                        <div className="temp-form-name">
-                            <label htmlFor="temp-change"></label>
-                            <input type="text" placeholder="Category" onChange={this.handleChange}/>
-                        </div>
-                        <div className="temp-form-name">
-                            <label htmlFor="temp-change"></label>
-                            <input type="text" placeholder="Image" onChange={this.handleChange}/>
-                        </div>
-                        <div className="temp-form-name">
-                            <label htmlFor="temp-change"></label>
-                            <textarea placeholder="Description" onChange={this.handleChange}></textarea>
-                        </div>
-                        <div className="temp-form-name">
-                            <label htmlFor="temp-change"></label>
-                            <input type="submit" value="New Program!" />
-                        </div>
-                    </form>
+                   {this.state.editing ? <EditForm 
+                                            handleUpdate= {this.handleUpdate} 
+                                            handleChange={this.handleChange}
+                                            formInputs={this.state.formInputs}
+                                            />  : <NewForm 
+                                            handleSubmit= {this.handleSubmit} 
+                                            handleChange={this.handleChange}
+                                            formInputs={this.state.formInputs}
+                                            /> }
                 </header>
 
                 <section>
                     <ul className="entries">
-                        {sites.map(site => {
+                        {this.state.sites.map(site => {
                             return (
                                 <li key={site.id}>
-                                    <button onClick={this.handleEdit} >Edit</button>
+                                    <button onClick={() => {this.handleEdit(site)}} >
+                                        {!this.state.editing ? "Edit" : "Cancel"}
+                                    </button>
                                     <button  onClick={this.handleDestroy}>Delete</button>
                                     <h1>{site.site_name}</h1>
                                     <figure>
